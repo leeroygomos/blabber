6dc96122beeeadf05226d1f4281ed81d8d341c18
Lee Roy Gomos <leeroy.gomos@gmail.com>
2023-04-25 00:56:27 -0700

    added mongodb connection

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+require("dotenv").config(); 
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -7,7 +8,37 @@ var cors = require("cors");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testAPIRouter = require("./routes/testAPI");
+const mongoose = require("mongoose");
+
 var app = express();
+
+mongoose.connect(
+  process.env.MONGODB_URL 
+);
+
+// uncomment this to test db connection
+//
+// const studentSchema = new mongoose.Schema({
+//   roll_no: Number,
+//   name: String,
+//   year: Number,
+//   subjects: [String]
+// });
+
+// const Student = mongoose.model('Student', studentSchema);
+
+// const stud = new Student({
+//   roll_no: 1001,
+//   name: 'Madison Hyde',
+//   year: 3,
+//   subjects: ['DBMS', 'OS', 'Graph Theory', 'Internet Programming']
+// });
+// stud
+//   .save()
+//   .then(
+//       () => console.log("One entry added"), 
+//       (err) => console.log(err)
+//   );
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
