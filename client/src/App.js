7f3e448449153c5ad8fff92dfbd38c52e445e29f
Lee Roy Gomos <leeroy.gomos@gmail.com>
2023-04-24 20:51:09 -0700

    connected express and react to electron

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState, useEffect} from 'react';
+
+
+
+function App() {
+
+  const [data, setData] = useState("");
+
+  useEffect(()=>{
+    fetch("http://localhost:3001/testAPI")
+      .then(res => res.text())
+      .then(res => {setData({apiResponse:res})})
+    },[]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {console.log(data)}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
