ca51ec56bc48538dbc3afd49eacd520b36dcf974
Lee Roy Gomos <leeroy.gomos@gmail.com>
2023-04-24 21:04:12 -0700

    fixing my mistake

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {console.log(data)}
+          {}
         </p>
         <a
           className="App-link"
