0dece11a9048968a9b98cf1cf9a820959a42b2e3
Lee Roy Gomos <leeroy.gomos@gmail.com>
2023-05-11 21:51:16 -0700

    added components

--- client/src/App.js
+++ client/src/App.js
@@ -1,64 +1,89 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import { socket } from './socket';
+import Chat from './components/Chat.js';
+import Friends from './components/Friends.js';
+import Groups from './components/Groups.js';
+import Input from './components/Input.js';
+import Layout from './components/Layout.js';
+import Message from './components/Message.js';
+import Messages from './components/Messages.js';
+import Profile from './components/Profile.js';
+import Sidebar from './components/Sidebar.js';
+import Tabs from './components/Tabs.js';
 
 function App() {
-  const [isConnected, setIsConnected] = useState(socket.connected);
-  const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState([]);
+  // const [isConnected, setIsConnected] = useState(socket.connected);
+  // const [message, setMessage] = useState('');
+  // const [messages, setMessages] = useState([]);
 
-  useEffect(() => {
-    function onConnect() {
-      setIsConnected(true);
-    }
+  // useEffect(() => {
+  //   function onConnect() {
+  //     setIsConnected(true);
+  //   }
 
-    function onDisconnect() {
-      setIsConnected(false);
-    }
+  //   function onDisconnect() {
+  //     setIsConnected(false);
+  //   }
 
-    function onShowMessage(msg) {
-      setMessages([...messages, msg]);
-    }
+  //   function onShowMessage(msg) {
+  //     setMessages([...messages, msg]);
+  //   }
 
-    socket.on('connect', onConnect);
-    socket.on('disconnect', onDisconnect);
-    socket.on('show message', (msg) => onShowMessage(msg));
+  //   socket.on('connect', onConnect);
+  //   socket.on('disconnect', onDisconnect);
+  //   socket.on('show message', (msg) => onShowMessage(msg));
 
-    return () => {
-      socket.off('connect', onConnect);
-      socket.off('disconnect', onDisconnect);
-    };
-  }, [messages]);
+  //   return () => {
+  //     socket.off('connect', onConnect);
+  //     socket.off('disconnect', onDisconnect);
+  //   };
+  // }, [messages]);
 
-  const sendMessage = () => {
-    console.log(message);
-    socket.emit('message', message);
-  };
+  // const sendMessage = () => {
+  //   console.log(message);
+  //   socket.emit('message', message);
+  // };
 
-  const handleInput = (event) => {
-    setMessage(event.target.value);
-  };
+  // const handleInput = (event) => {
+  //   setMessage(event.target.value);
+  // };
 
   return (
-    <div>
-      {isConnected ? <p>Server is connected</p> : <p>Server bad</p>}
-      <div>
-        <ul>
-          {messages.map((msg, index) => {
-            return <li key={index}>{msg}</li>;
-          })}
-        </ul>
-      </div>
-      <div>
-        <input
-          type="text"
-          onChange={(event) => {
-            handleInput(event);
-          }}
-        />
-        <button onClick={() => sendMessage()}>Send</button>
-      </div>
-    </div>
+    // <div>
+    //   {isConnected ? <p>Server is connected</p> : <p>Server bad</p>}
+    //   <div>
+    //     <ul>
+    //       {messages.map((msg, index) => {
+    //         return <li key={index}>{msg}</li>;
+    //       })}
+    //     </ul>
+    //   </div>
+    //   <div>
+    //     <input
+    //       type="text"
+    //       onChange={(event) => {
+    //         handleInput(event);
+    //       }}
+    //     />
+    //     <button onClick={() => sendMessage()}>Send</button>
+    //   </div>
+    // </div>
+
+    <Layout>
+        <Sidebar>
+          <Profile></Profile>
+          <Friends></Friends>
+          <Groups></Groups>
+        </Sidebar>
+        <Tabs></Tabs>
+        <Chat>
+          <Messages>
+            <Message></Message>
+          </Messages>
+          <Input></Input>
+        </Chat>
+    </Layout>
   );
 }
 
