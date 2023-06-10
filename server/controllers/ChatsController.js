6236c3a3fc3281d3e4dd26ca38a7e297ed5dd8bd
Laura Lu <lulaura011205@gmail.com>
2023-06-10 19:57:57 -0400

    updated getFriends

--- server/controllers/ChatsController.js
+++ server/controllers/ChatsController.js
@@ -3,7 +3,17 @@ const Users = require('../models/Users');
 
 module.exports = {
   // get all friends (DMs) from current session
-  getFriends: async (req, res) => {},
+  getFriends: async (req, res) => {
+    // get all group chat IDs from current session
+    const chatIds = req.session.chatIds;
+    const curUserId = req.session.id;
+
+    // find all DMs in current session
+    let directMessages = await Chats.find({ isGroupChat: false })
+      .where('_id')
+      .in(chatIds)
+      .exec();
+  },
 
   // get all group chats from current session
   getGroups: async (req, res) => {
