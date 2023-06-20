5bcdd5b4c3a2629db310b1692172ec48b14231ca
Laura Lu <lulaura011205@gmail.com>
2023-06-20 00:36:37 -0400

    added avatar upload api

--- server/controllers/UsersController.js
+++ server/controllers/UsersController.js
@@ -3,124 +3,156 @@ const auth = require('../services/AuthenticationService');
 const usersService = require('../services/UsersService');
 const chatsService = require('../services/ChatsService');
 
-async function login(username, password, req, res){
-    // check if a user is already logged in
-    if (req.session?.username){
-        res.redirect('/');
-        return;
-    }
-
-    // check if user with the given username exists
-    let user = await usersService.getUserByUsername(username);
-    if (user) {
-        // check if password is correct
-        if (await auth.validateUser(password, user.password)){
-            // set session variables
-            req.session.userid = user._id;
-            req.session.username = user.username;
-            req.session.name = user.name;
-            req.session.chatIds = user.chatIds;
-
-            // update user status to online
-            user.status = 'online';
-            await user.save();
-
-            //redirect to root page
-            res.status(200).redirect('/')
-        }
-        else{
-            res.status(401).send("Incorrect username or password!")
-        }
-    }
-    else {
-        res.status(401).send("Incorrect username or password!");
-    }
-}
-
-async function signup(data, res){
-    // check if a user with the username already exists
-    let user = await usersService.getUserByUsername(data.username);
-    if (user){
-        res.status(401).send({message: "Username already taken!"});
-        return;
-    }
-    // hash password and create new user
-    hashedPassword = await auth.hashPassword(data.password);
-    const newUser = Users({
-        email: data.email,
-        username: data.username,
-        password: hashedPassword,
-        bio: "",
-    });
-
-    // save user then redirect to root page if successful
-    newUser.save().then(
-        () => {
-            res.status(200).redirect('/');
-        },
-        (err) => {
-            console.log(err);
-            res.status(500).send({message: 'Internal Server Error'});
-        }
-    );
-}
-
-async function logout(req,res){
-    // set status to offline
-    let user = await usersService.getUserByUsername(req.session.username);
-    user.status = 'offline';
-    await user.save();
-
-    // destroy session and redirect to root page
-    req.session.destroy();
+async function login(username, password, req, res) {
+  // check if a user is already logged in
+  if (req.session?.username) {
     res.redirect('/');
+    return;
+  }
+
+  // check if user with the given username exists
+  let user = await usersService.getUserByUsername(username);
+  if (user) {
+    // check if password is correct
+    if (await auth.validateUser(password, user.password)) {
+      // set session variables
+      req.session.userid = user._id;
+      req.session.username = user.username;
+      req.session.name = user.name;
+      req.session.chatIds = user.chatIds;
+
+      // update user status to online
+      user.status = 'online';
+      await user.save();
+
+      //redirect to root page
+      res.status(200).redirect('/');
+    } else {
+      res.status(401).send('Incorrect username or password!');
+    }
+  } else {
+    res.status(401).send('Incorrect username or password!');
+  }
 }
 
-async function addFriend(req, res){
-    // check if a user with the given username exists
-    let friend = await usersService.getUserByUsername(req.params.username);
-    if (!friend){
-        res.send("User not found!");
-        return;
-    }
-    // check if user is you
-    else if (friend.username === req.session.username){
-        res.send("You cannot befriend yourself");
-        return;
-    }
+async function signup(data, res) {
+  // check if a user with the username already exists
+  let user = await usersService.getUserByUsername(data.username);
+  if (user) {
+    res.status(401).send({ message: 'Username already taken!' });
+    return;
+  }
+  // hash password and create new user
+  hashedPassword = await auth.hashPassword(data.password);
+  const newUser = Users({
+    email: data.email,
+    username: data.username,
+    password: hashedPassword,
+    bio: '',
+  });
 
-    // check if user is already friends
-    let user = await usersService.getUserByUsername(req.session.username);
-    if (user.friendsList.includes(friend._id)){
-        res.status(401).send("You are already friends");
-        return;
+  // save user then redirect to root page if successful
+  newUser.save().then(
+    () => {
+      res.status(200).redirect('/');
+    },
+    (err) => {
+      console.log(err);
+      res.status(500).send({ message: 'Internal Server Error' });
     }
-    else {
-        // create direct message between user and friend
-        try {
-            await chatsService.createDirectMessage(friend._id, user._id);
-            user.friendsList.push(friend._id);
-            friend.friendsList.push(user._id);
-            await user.save();
-            await friend.save();
-            res.status(200).send("Friend added");
-        }
-        catch(err){
-            console.log(err);
-            res.status(500).send('Internal Server Error');
-            return;
-        }
-    }
+  );
 }
 
-function getLoggedInUser(req, res){
-    res.json({id: req.session?.userid, username: req.session?.username});
+async function logout(req, res) {
+  // set status to offline
+  let user = await usersService.getUserByUsername(req.session.username);
+  user.status = 'offline';
+  await user.save();
+
+  // destroy session and redirect to root page
+  req.session.destroy();
+  res.redirect('/');
+}
+
+async function addFriend(req, res) {
+  // check if a user with the given username exists
+  let friend = await usersService.getUserByUsername(req.params.username);
+  if (!friend) {
+    res.send('User not found!');
+    return;
+  }
+  // check if user is you
+  else if (friend.username === req.session.username) {
+    res.send('You cannot befriend yourself');
+    return;
+  }
+
+  // check if user is already friends
+  let user = await usersService.getUserByUsername(req.session.username);
+  if (user.friendsList.includes(friend._id)) {
+    res.status(401).send('You are already friends');
+    return;
+  } else {
+    // create direct message between user and friend
+    try {
+      await chatsService.createDirectMessage(friend._id, user._id);
+      user.friendsList.push(friend._id);
+      friend.friendsList.push(user._id);
+      await user.save();
+      await friend.save();
+      res.status(200).send('Friend added');
+    } catch (err) {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+  }
+}
+
+function getLoggedInUser(req, res) {
+  res.json({ id: req.session?.userid, username: req.session?.username });
+}
+
+async function uploadAvatar(req, res) {
+  // Check if request is authorized
+  if (!req.session) {
+    res.status(401).send('Unauthorized');
+    return;
+  }
+
+  // update currently logged in user's avatar
+  try {
+    const filter = { _id: req.session?.userid };
+    const update = req.body;
+    await Users.findOneAndUpdate(filter, update);
+    res.status(201).json('Avatar Uploaded!');
+  } catch (error) {
+    res.status(500).send('Internal Server Error');
+  }
+}
+
+async function getAvatar(req, res) {
+  // Check if request is authorized
+  if (!req.session) {
+    res.status(401).send('Unauthorized');
+    return;
+  }
+
+  // get currently logged in user's avatar
+  try {
+    const user = await Users.find({ _id: req.session.userid });
+    res.json(user.avatar);
+  } catch (error) {
+    res.status(500).send('Internal Server Error');
+  }
 }
 
 module.exports = {
-    login, 
-    signup, 
-    logout, 
-    addFriend, 
-    getLoggedInUser
+  login,
+  signup,
+  logout,
+  addFriend,
+  getLoggedInUser,
+  uploadAvatar,
+  getAvatar,
 };
--- server/routes/UsersRoutes.js
+++ server/routes/UsersRoutes.js
@@ -1,30 +1,37 @@
 usersController = require('../controllers/UsersController');
 
-module.exports = function(app){
-    app.post('/users/login', function(req, res){
-        data = req.body;
-        usersController.login(data.username, data.password, req, res);
-    });
+module.exports = function (app) {
+  app.post('/users/login', function (req, res) {
+    data = req.body;
+    usersController.login(data.username, data.password, req, res);
+  });
 
-    app.post('/users/signup', function(req, res){
-        data = req.body;
-        usersController.signup(data,res);
-    });
+  app.post('/users/signup', function (req, res) {
+    data = req.body;
+    usersController.signup(data, res);
+  });
 
-    app.post('/users/logout', function(req, res){
-        usersController.logout(req,res);
-    });
+  app.post('/users/logout', function (req, res) {
+    usersController.logout(req, res);
+  });
 
-    app.post('/users/addFriend/:username', function(req, res){
-        usersController.addFriend(req,res);
-    })
+  app.post('/users/addFriend/:username', function (req, res) {
+    usersController.addFriend(req, res);
+  });
 
-    app.get('/users/getLoggedInUser', function(req, res){
-        usersController.getLoggedInUser(req,res);
-    })
+  app.get('/users/getLoggedInUser', function (req, res) {
+    usersController.getLoggedInUser(req, res);
+  });
 
-    app.post('/users/addFriend/:username', function(req, res){
-        usersController.addFriend(req,res);
-    })
+  app.post('/users/addFriend/:username', function (req, res) {
+    usersController.addFriend(req, res);
+  });
 
-}
+  app.post('/users/uploadAvatar', function (req, res) {
+    usersController.uploadAvatar(req, res);
+  });
+
+  app.get('/users/getAvatar', function (req, res) {
+    usersController.getAvatar(req, res);
+  });
+};
