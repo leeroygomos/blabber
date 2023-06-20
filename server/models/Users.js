acc671de1a7fc511f8de0685229852aad1469e2b
Laura Lu <lulaura011205@gmail.com>
2023-06-20 00:36:05 -0400

    added string-based avatar field

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,5 +1,5 @@
 // Require Mongoose
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 // Define schema
 const Schema = mongoose.Schema;
@@ -7,7 +7,7 @@ const Schema = mongoose.Schema;
 const UsersSchema = new Schema({
   username: {
     type: String,
-    unique: true
+    unique: true,
   },
   password: String,
   name: String,
@@ -17,13 +17,14 @@ const UsersSchema = new Schema({
   status: {
     type: String,
     default: 'offline',
-    enum: ['online', 'offline']
+    enum: ['online', 'offline'],
   },
-  bio: String
+  bio: String,
+  avatar: String,
 });
 
 // Compile model from schema
-const Users = mongoose.model("Users", UsersSchema);
+const Users = mongoose.model('Users', UsersSchema);
 
 // export { Users };
 module.exports = Users;
