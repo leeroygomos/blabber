2f9f654e4587bac3fdc56ae8465343f6e53d952b
Laura Lu <lulaura011205@gmail.com>
2023-06-21 22:36:47 -0400

    added message schema

---
+++ server/models/Messages.js
@@ -0,0 +1,24 @@
+// Require Mongoose
+const mongoose = require('mongoose');
+
+// Define schema
+const Schema = mongoose.Schema;
+
+const MessagesSchema = new Schema(
+  {
+    chatId: {
+      type: String,
+      minLength: 1,
+      maxLength: 4096,
+    },
+    senderId: String,
+    message: String,
+  },
+  { timestamps: true }
+);
+
+// Compile model from schema
+const Messages = mongoose.model('Messages', MessagesSchema);
+
+// export { Messages };
+module.exports = Messages;
