e93417c6c2b4b2a49427b32cfdea7179a318d115
Laura Lu <lulaura011205@gmail.com>
2023-06-24 17:59:04 -0400

    added get user by email

--- server/services/UsersService.js
+++ server/services/UsersService.js
@@ -1,36 +1,41 @@
 const Users = require('../models/Users');
 
-async function getUserByUsername(username){
-    let user = await Users.findOne({username: username}).exec();
-    return user;
+async function getUserByUsername(username) {
+  let user = await Users.findOne({ username: username }).exec();
+  return user;
 }
 
-async function getUsersByUsernames(usernames){
-    let users = await Users.find({username: {$in: usernames}}).exec();
-    return users;
+async function getUsersByUsernames(usernames) {
+  let users = await Users.find({ username: { $in: usernames } }).exec();
+  return users;
 }
 
-async function getUsersByIds(ids){
-    let users = await Users.find({_id: {$in: ids}}).exec();
-    return users;
+async function getUserByEmail(email) {
+  let user = await Users.findOne({ email: email }).exec();
+  return user;
 }
 
-async function updateChatLists(chatId, userIds){
-    let users = await getUsersByIds(userIds);
-    for (let i = 0; i < users.length; i++){
-        users[i].chatIds.push(chatId);
-        try{
-            await users[i].save();
-        }
-        catch(err){
-            throw new Error('Error updating chat list for user '+ users[i].username);
-        }
+async function getUsersByIds(ids) {
+  let users = await Users.find({ _id: { $in: ids } }).exec();
+  return users;
+}
+
+async function updateChatLists(chatId, userIds) {
+  let users = await getUsersByIds(userIds);
+  for (let i = 0; i < users.length; i++) {
+    users[i].chatIds.push(chatId);
+    try {
+      await users[i].save();
+    } catch (err) {
+      throw new Error('Error updating chat list for user ' + users[i].username);
     }
+  }
 }
 
 module.exports = {
-    getUserByUsername, 
-    getUsersByUsernames, 
-    getUsersByIds,
-    updateChatLists
+  getUserByUsername,
+  getUsersByUsernames,
+  getUserByEmail,
+  getUsersByIds,
+  updateChatLists,
 };
