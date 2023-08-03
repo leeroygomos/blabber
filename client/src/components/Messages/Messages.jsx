ab081146743921c1c5dab02ca8bb1fbef2f2ef3a
Laura Lu <lulaura011205@gmail.com>
2023-08-02 22:19:36 -0400

    updated messages display template

--- client/src/components/Messages/Messages.jsx
+++ client/src/components/Messages/Messages.jsx
@@ -8,7 +8,7 @@ export default function Messages({messages}){
             <div className='messages'>
                 {messages.length > 0 
                     ? messages.map((message) => {
-                            return <MessageItem username={message.username} message={message.message} timestamp={message.timestamp}/>
+                            return <MessageItem username={message.senderName} message={message.message} timestamp={message.createdAt}/>
                         }) 
                     : <p className="placeholder">No messages</p>
                 }
@@ -19,11 +19,14 @@ export default function Messages({messages}){
 }
 
 function MessageItem({username, message, timestamp}){
+    let date = new Date(timestamp);
+    let formattedTimestamp = date.toLocaleDateString('en-US', {timeZone: 'America/Vancouver'}) + ' ' + date.toLocaleTimeString('en-US', {timeZone: 'America/Vancouver'});
+
     return(
         <div className='message-item'>
             <div className='message-item-header'>
                 <p className='user'>{username}</p>
-                <p className='timestamp'>{timestamp}</p>
+                <p className='timestamp'>{formattedTimestamp}</p>
             </div>
             <p classname='message'>{message}</p>
         </div>
