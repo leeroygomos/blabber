562b64fe210e738370f7cc23a7228cc198d384b4
Laura Lu <lulaura011205@gmail.com>
2023-08-02 22:17:44 -0400

    updated props to tabs

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import './Chat.css'
 import Tabs from '../Tabs/Tabs';
 
-export default function Chat({activeChats}){
+export default function Chat({activeChats, currentTab, setCurrentTab, messages, updateMessages }){
     return(
         <div className="chat">
             {
                 activeChats.length > 0 
                 ? 
-                <Tabs activeChats={activeChats}/>
+                <Tabs activeChats={activeChats}
+                      currentTab={currentTab} 
+                      setCurrentTab={setCurrentTab}
+                      messages={messages}
+                      updateMessages={updateMessages}/>
                 : 
                 <div className='placeholder'>
                     <p>You have no friends</p>
